b86bb1be24f9f45e87fbe48442e775f6449be5d7
Magairisarri <magairisarri@gmail.com>
2026-06-19 12:24:14 -0300

    Remove updateUserPartial function
    Removed the updateUserPartial function from UserController.

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -38,19 +38,6 @@ export const updateUser = (req: Request, res: Response): void => {
   res.status(200).json(users[userIndex]);
 };
 
-export const updateUserPartial = (req: Request, res: Response): void => {
-  const dni = parseInt(req.params['dni'] as string);
-  const index = users.findIndex(u => u.dni === dni);
-
-  if (index === -1) {
-    res.status(404).json({ mensaje: 'Usuario no encontrado' });
-    return;
-  }
-
-  users[index] = { ...users[index], ...req.body }; // solo pisa los campos enviados
-  res.status(200).json(users[index]);
-};
-
 export const deleteUser = (req: Request, res: Response): void => {
   const dni = parseInt(req.params['dni'] as string);
   const user = users.findIndex(u => u.dni === dni);
