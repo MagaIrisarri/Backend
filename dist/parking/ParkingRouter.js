99b165ac4031eb3c65fd903af4be24bebb196f3f
Magairisarri <magairisarri@gmail.com>
2026-07-01 22:54:46 -0300

    Dejar de trackear dist y tsbuildinfo (archivos generados)

--- dist/parking/ParkingRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { add, findAll, findOneById, update, remove, } from './ParkingController.js';
-export const parkingRouter = Router();
-parkingRouter.post('/', add);
-parkingRouter.get('/', findAll);
-parkingRouter.get('/:id', findOneById);
-parkingRouter.put('/:id', update);
-parkingRouter.delete('/:id', remove);
-//# sourceMappingURL=ParkingRouter.js.map
--- dist/routes/UserRoute.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { createUser, getUsers, getUserDNI, updateUser } from '../controllers/UserController.js';
-const router = Router();
-router.post('/', createUser);
-router.get('/', getUsers);
-router.get('/:dni', getUserDNI);
-router.put('/:dni', updateUser);
-export default router;
-//# sourceMappingURL=UserRoute.js.map
