850449a54820409eff92df27465635a832b9ec93
Magairisarri <magairisarri@gmail.com>
2026-07-01 22:50:16 -0300

    Update import path for UserController

--- src/User/UserRoute.ts
+++ src/User/UserRoute.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { createUser, getUsers, getUserDNI, updateUser, deleteUser } from '../UserController.ts';
+import { createUser, getUsers, getUserDNI, updateUser, deleteUser } from './UserController.js';
 
 const router = Router();
 
